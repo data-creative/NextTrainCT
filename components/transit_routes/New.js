9690ed4e3de60f906902b6ed662a36e21cbd5ba8
MJ Rossetti <datacreativellc@gmail.com>
2016-10-10 00:33:52 -0400

    Add static station-picker dropdowns

--- components/transit_routes/New.js
+++ components/transit_routes/New.js
@@ -1,8 +1,18 @@
+
+
 import React, {Component} from 'react';
 import {Text} from 'react-native';
-import {Container, Header, Button, Icon, Title, Content} from 'native-base';
+import {Container, Header, Button, Icon, Title, Content, Picker} from 'native-base';
 
 export default class NewTransitRoute extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOrigin: 'key2',
+      selectedDestination: 'key3',
+    }
+  }
+
   render() {
     const goBack = this.goBack;
     const navigator = this.props.navigator;
@@ -16,15 +26,59 @@ export default class NewTransitRoute extends Component {
         </Header>
 
         <Content style={{margin:20}}>
-          <Text style={{marginTop:5, marginBottom:5}}>TODO: ORIGIN-STATION PICKER</Text>
-          <Text style={{marginTop:5, marginBottom:5}}>TODO: DESTINATION-STATION PICKER</Text>
-          <Button block primary style={{marginTop:5}}> Submit </Button>
+
+          <Text style={{fontWeight:'bold', fontSize:16}}>
+            {"ORIGIN STATION:"}
+          </Text>
+          <Picker style={{marginTop:5, marginBottom:5}}
+                  mode="dropdown"
+                  selectedValue={this.state.selectedOrigin}
+                  onValueChange={this.onOriginValueChange.bind(this)}>
+            <Picker.Item selected label="Branford" value="key0" />
+            <Picker.Item label="Guilford" value="key1" />
+            <Picker.Item label="New Haven Union" value="key2" />
+            <Picker.Item label="New Haven State" value="key3" />
+          </Picker>
+
+          <Text style={{marginTop:15, fontWeight:'bold', fontSize:16}}>
+            {"DESTINATION STATION:"}
+          </Text>
+          <Picker style={{marginTop:5, marginBottom:5}}
+                  mode="dropdown"
+                  selectedValue={this.state.selectedDestination}
+                  onValueChange={this.onDestinationValueChange.bind(this)}>
+            <Picker.Item label="Hello" value="key0" />
+            <Picker.Item label="Dogs" value="key1" />
+            <Picker.Item selected label="Birds" value="key2" />
+            <Picker.Item label="Elephants" value="key3" />
+          </Picker>
+
+          <Button block primary style={{marginTop:15}}>
+            Save
+          </Button>
         </Content>
       </Container>
     );
   }
 
+  onOriginValueChange(val){
+    this.setState({selectedOrigin: val});
+  }
+
+  onDestinationValueChange(val){
+    this.setState({selectedDestination: val});
+  }
+
   goBack(navigator){
     navigator.pop();
   }
+
+  componentWillMount(){  console.log("NEW WILL MOUNT")  }
+  componentDidMount(){  console.log("NEW DID MOUNT")  }
+  componentWillReceiveProps(nextProps){  console.log("NEW WILL RECEIVE PROPS")  }
+  componentWillUpdate(nextProps, nextState){
+    console.log("NEW WILL UPDATE", nextState)
+  }
+  componentDidUpdate(prevProps, prevState){  console.log("NEW DID UPDATE")  }
+  componentWillUnmount(){  console.log("NEW WILL UNMOUNT")  }
 };
