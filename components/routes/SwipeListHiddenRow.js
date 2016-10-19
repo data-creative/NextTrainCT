93a9b4a9375b3c65b8bbbe69b78bfe52b67fcaec
MJ Rossetti <datacreativellc@gmail.com>
2016-10-19 19:59:15 -0400

    Demonstrate ability to trigger function on delete button press

--- components/routes/SwipeListHiddenRow.js
+++ components/routes/SwipeListHiddenRow.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Alert} from 'react-native';
+import {Button, Icon} from 'native-base';
 
 export default class SwipeListRowHidden extends Component {
+  constructor(props){
+    super(props)
+    this.handleDeleteButtonPress = this.handleDeleteButtonPress.bind(this);
+  }
+
   render(){
-    const DeleteButton = <Text>Delete!</Text>
+    const DeleteButton = (
+      <Button danger onPress={this.handleDeleteButtonPress}>
+        <Icon name="md-trash" />
+      </Button>
+    )
 
     return (
       <View style={styles.hiddenRow}>
         {DeleteButton}
         {DeleteButton}
       </View>
+    )
+  }
+
+  handleDeleteButtonPress(){
+    console.log("DELETE BUTTON PRESSED")
+
+    Alert.alert(
+      "Remove from favorites?",
+      "Are you sure you want to remove this route from your favorites list?",
+      [
+        {text: 'Cancel', onPress: function(){  console.log("DELETE NEVERMIND")  }},
+        {text: 'OK', onPress: function(){  console.log("DELETE OK")  }},
+      ]
     )
   }
 }
