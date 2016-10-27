21b633f7a772d39a2cff6d750b095486e9039863
MJ Rossetti <datacreativellc@gmail.com>
2016-10-26 23:30:28 -0400

    Try suggested solution

--- components/examples/MyComponent.js
+++ components/examples/MyComponent.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import ReactHeight from 'react-height';
 
-export default class MyComponent extends Component {
-  render(){
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 0,
+    };
+    this.onHeightReady = this.onHeightReady.bind(this);
+  }
+
+  onHeightReady(height) {
+    this.setState({ height });
+  }
+
+  render() {
     return (
-      <ReactHeight onHeightReady={height => console.log(height)}>
+      <ReactHeight onHeightReady={function(height){
+          console.log("HEIGHT IS READY:", height)
+        }}>
         <View>
           <Text>Hello world</Text>
         </View>
       </ReactHeight>
-    )
+    );
   }
 }
+
+export default MyComponent;
