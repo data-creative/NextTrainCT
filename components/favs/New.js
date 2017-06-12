955000d14b8cdb79253ac5cb0f94b92a07b65dfb
MJ Rossetti <datacreativellc@gmail.com>
2017-06-11 20:55:10 -0400

    Fix default station issue

--- components/favs/New.js
+++ components/favs/New.js
@@ -6,7 +6,7 @@ import StationPicker from "../stations/Picker"
 export default class NewFav extends Component {
   constructor(props) {
     super(props);
-    this.state = {origin: 'GCS', destination: 'NHV', displaySpinner:false};
+    this.state = {origin: 'BRN', destination: 'NHV', displaySpinner:false};
     this.navigator = this.props.navigator;
     this.favs = this.props.favs;
     this.goBack = this.goBack.bind(this);
