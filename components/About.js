7e0feb5aabba428165d0f92a9bb88ea0de0a8580
MJ Rossetti <datacreativellc@gmail.com>
2017-06-11 20:06:47 -0400

    Edit about page and restrict stations

--- components/About.js
+++ components/About.js
@@ -27,15 +27,23 @@ export default class About extends Component {
 
         <Content style={styles.content}>
 
-          <Title style={styles.heading}>About</Title>
+          <Title style={styles.firstHeading}>About</Title>
           <Text style={styles.text}>
             Search Shore Line East transit schedules in a fast and easy way.
-            Save a route to your favorites, then click a button to find out when the next train is coming.
+            Save a route to your favorites, then find out when the next train is coming anytime with the click of a button.
+          </Text>
+
+          <Title style={styles.heading}>Disclaimer</Title>
+          <Text style={styles.text}>
+              This app relies on Shore Line East schedule data published online by CTtransit.
+              The app developer is not affiliated with Shore Line East or CTtransit, and cannot guarantee the accuracy of their data.
+              If you notice an issue with the schedule data, please contact the developer, who will notify CTtransit.
           </Text>
 
           <Title style={styles.heading}>Contact</Title>
           <Text style={styles.text}>
-            This app was made by MJ Rossetti, a native of Branford, CT, and is provided as a free service by Data Creative, LLC.
+            This app was made by MJ Rossetti, a native of Branford, CT.
+            It relies on a backend web server which is operated by his software development company, Data Creative, as a free public service.
           </Text>
           <List>
             <ListItem iconLeft style={styles.listItem}>
@@ -54,8 +62,8 @@ export default class About extends Component {
 
           <Title style={styles.heading}>Donate</Title>
           <Text style={styles.text}>
-            If you appreciate this app, please consider donating.
-            Your donations keep the server running!
+            If you like this app, please consider donating.
+            Your donations keep the servers running!
           </Text>
           <List>
             <ListItem iconLeft style={styles.listItem}>
@@ -69,29 +77,23 @@ export default class About extends Component {
           <Title style={styles.heading}>Contribute</Title>
           <Text style={styles.text}>
             This app is powered by open data and open source software.
-            Your contributions are welcome!
+            If you know how to submit a PR or file an issue on GitHub, your contributions are welcome!
           </Text>
           <List>
-            <ListItem iconLeft style={styles.listItem}>
-              <Button transparent onPress={this.goServerRepo}>
-                <Icon name='logo-github' style={styles.listItemIcon}/>
-                <Text style={styles.listItemText}>Contribute to the API</Text>
-              </Button>
-            </ListItem>
             <ListItem iconLeft style={styles.listItem}>
               <Button transparent onPress={this.goClientRepo}>
                 <Icon name='logo-github' style={styles.listItemIcon}/>
                 <Text style={styles.listItemText}>Contribute to the mobile app</Text>
               </Button>
             </ListItem>
+            <ListItem iconLeft style={styles.listItem}>
+              <Button transparent onPress={this.goServerRepo}>
+                <Icon name='logo-github' style={styles.listItemIcon}/>
+                <Text style={styles.listItemText}>Contribute to the web service</Text>
+              </Button>
+            </ListItem>
           </List>
 
-          <Title style={styles.heading}>Disclaimer</Title>
-          <Text style={styles.text}>
-              This app relies on Shore Line East schedule data published online by CTtransit.
-              The developer is not affiliated with Shore Line East or CTtransit, and therefore cannot guarantee the accuracy of their schedule data.
-              However, if you notice an issue with the schedule data, please contact the developer, who can help bring the matter to the attention of CTtransit and ask them to fix it.
-          </Text>
 
         </Content>
 
@@ -139,6 +141,7 @@ About.propTypes = {
 const styles = StyleSheet.create({
   content: {margin: 20},
   text: {marginTop: 10},
+  firstHeading: {color: "#7a7a7a"},
   heading: {color: "#7a7a7a", marginTop: 10},
   listItem: {height:60},
   listItemIcon: {marginRight:13, color:'#282828'},
